c86c9b230b60d6e45f6fdd502d93e1c1b5161927
dhairya1899 <51793626+dhairya1899@users.noreply.github.com>
2020-02-05 01:08:54 +0530

    Delete try.py

--- try.py
+++
@@ -1,8 +0,0 @@
-newfile = ""
-filename = "C:/Users/DHAIRYA/Downloads/DetailedStatement.psv"
-for i in filename:
-    if (i == '/'):
-        newfile += '\\\\'
-    else:
-        newfile += i
-print(newfile)
